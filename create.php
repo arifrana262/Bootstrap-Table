c60a6209f9dce99afeda73ea1c74f30ba0c2c450
arifrana262 <arifur.stu2017@juniv.edu>
2022-09-04 02:33:58 +0600

    minor changes

--- create.php
+++ create.php
@@ -22,6 +22,6 @@ if (mysqli_query($conn, $sql)) {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
 
-// mysqli_close($conn);
+ mysqli_close($conn);
 
-// $conn->close();
+ $conn->close();
